0cb55e84d431a05ae99f893b25b4e08f39416371
Amogh_L <155659945+0kayBr0@users.noreply.github.com>
2025-02-03 21:56:27 +0530

    Update App.js

--- Commit-to-Cloud-github-and-hosting-simplyfied-main/src/App.js
+++ Commit-to-Cloud-github-and-hosting-simplyfied-main/src/App.js
@@ -6,8 +6,8 @@ function App() {
       <header className="App-header">
         <h1>Peer-Mentor Learning Series Workshop 1</h1>
         <div className="user-info">
-          <p><strong>Name:</strong> Your Name</p> 
-          <p><strong>USN:</strong> Your USN</p>
+          <p><strong>Name:</strong> Amogh L </p> 
+          <p><strong>USN:</strong> 1RVU23CSE043 </p>
         </div>
       </header>
     </div>
